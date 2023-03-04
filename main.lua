bc157b1944c49e03f0c84ccc6828743fc25107bb
elvodqa <emircengiz21@gmail.com>
2023-03-04 21:54:31 +0300

    Change main

--- main.lua
+++ main.lua
@@ -18,9 +18,12 @@ function love.draw()
       
         love.graphics.draw(saul, 0, 0)
 
-        -- draw neds here -- 
+        -- draw ends here -- 
+        love.graphics.setLineWidth(2)
         love.graphics.rectangle("line", 0, 0, 480, 360) --border
     love.graphics.setCanvas()
-    love.graphics.draw(gameCanvas, 20, 20, 0, canvasScale, canvasScale)
+    sX, sY = love.graphics.getDimensions()
+    canvasScale = math.min(sX/gameCanvas:getWidth(), sY/gameCanvas:getHeight())
+    love.graphics.draw(gameCanvas, sX/2, sY/2, 0, canvasScale, canvasScale, gameCanvas:getWidth()/2, gameCanvas:getHeight()/2)
 
 end
