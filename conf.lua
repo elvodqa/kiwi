bd6d16d864f5ed45cdec8c8607a33c10bb7ee47f
egegl <gurege.24@robcol.k12.tr>
2023-03-04 21:39:44 +0300

    a

--- conf.lua
+++ conf.lua
@@ -1,11 +1,6 @@
 function love.conf(t)
     t.window.highdpi = true
-<<<<<<< HEAD
-
-    
-=======
     t.window.resizable = true
     t.window.title = "Kiwi"
     t.window.icon = "assets/images/Goodman.png"
->>>>>>> ffdcb2d34b53a4ab2267a53c3e0be296dc696957
 end
