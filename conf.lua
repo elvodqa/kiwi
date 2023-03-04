ffdcb2d34b53a4ab2267a53c3e0be296dc696957
elvodqa <emircengiz21@gmail.com>
2023-03-04 21:37:47 +0300

    Add logo

--- conf.lua
+++ conf.lua
@@ -1,3 +1,6 @@
-function love.config(t)
+function love.conf(t)
     t.window.highdpi = true
+    t.window.resizable = true
+    t.window.title = "Kiwi"
+    t.window.icon = "assets/images/Goodman.png"
 end
