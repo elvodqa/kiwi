8d43d7c5d5cbc721da6fbe43efc4e2d2bce19e26
elvodqa <emircengiz21@gmail.com>
2023-03-05 13:43:03 +0300

    Add ui canvas

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 
 local gameCanvas = love.graphics.newCanvas(480, 360)
+local uiCanvas = love.graphics.newCanvas(480*2, 360*2)
 local canvasScale = 1
 
 
@@ -17,13 +18,27 @@ function love.draw()
         -- draw goes here -- 
       
         love.graphics.draw(saul, 0, 0)
-
+        
         -- draw ends here -- 
-        love.graphics.setLineWidth(2)
+        love.graphics.setLineWidth(1)
         love.graphics.rectangle("line", 0, 0, 480, 360) --border
+    love.graphics.setCanvas(uiCanvas)
+        love.graphics.clear()
+        -- ui goes here -- 
+        
+        love.graphics.print("Points: 0", 0, 0)
+
+        -- ui ends here -- 
+        love.graphics.setLineWidth(1)
+        love.graphics.rectangle("line", 0, 0, 480*2, 360*2) --border
     love.graphics.setCanvas()
+
+
+    -- draw canvases
     local sX, sY = love.graphics.getDimensions()
-    canvasScale = math.min(sX/gameCanvas:getWidth(), sY/gameCanvas:getHeight())
-    love.graphics.draw(gameCanvas, sX/2, sY/2, 0, canvasScale, canvasScale, gameCanvas:getWidth()/2, gameCanvas:getHeight()/2)
+    local gameCanvasScale = math.min(sX/gameCanvas:getWidth(), sY/gameCanvas:getHeight())
+    love.graphics.draw(gameCanvas, sX/2, sY/2, 0, gameCanvasScale, gameCanvasScale, gameCanvas:getWidth()/2, gameCanvas:getHeight()/2)
+    local uiCanvasScale = math.min(sX/uiCanvas:getWidth(), sY/uiCanvas:getHeight())
+    love.graphics.draw(uiCanvas, sX/2, sY/2, 0, uiCanvasScale, uiCanvasScale, uiCanvas:getWidth()/2, uiCanvas:getHeight()/2)
 
 end
